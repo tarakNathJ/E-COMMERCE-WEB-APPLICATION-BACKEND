041762f5ac902469362edc0827d4eeb89d6dd2d3
Tarak Nath Jana <janatarak739@gmail.com>
2024-09-29 12:48:05 +0530

    change status Code

--- Controllers/ProductManager.js
+++ Controllers/ProductManager.js
@@ -377,7 +377,7 @@ exports.ProductVariantsController = async(req, res) => {
             StorageLocation: StorageLocation,
         })
 
-        return res.status(200).json({
+        return res.status(201).json({
             success: true,
             message: "success fully create product",
             creatVarient
